138bf62a33ba33ced0fb6574c790c914d21f2da5
Adrian Gonzales <adriangonzalesval@gmail.com>
2026-09-07 13:34:41 -0500

    ci: Updated ci java version to 25

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -16,11 +16,11 @@ dependencies {
 tasks {
     compileJava {
         options.encoding = Charsets.UTF_8.name()
-        options.release.set(21)
+        options.release.set(25)
     }
     runVelocity {
         velocityVersion(libs.versions.velocity.get())
     }
 }
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(25))
